0052ac3b62de6ff7c917f2565da72b1d814d1c35
d0p3t <remcotroost@outlook.com>
2018-12-13 14:06:30 +0100

    v1.0.0

---
+++ Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace TurnThatShitUpM
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            var lastVehicle = Game.PlayerPed.LastVehicle;
+            
+            if (lastVehicle == null) { return; }
+
+            if (lastVehicle.IsEngineRunning)
+            {
+                SetVehicleRadioLoud(lastVehicle.Handle, true);
+            }
+            
+            await Delay(0);
+        }
+    }
+}
